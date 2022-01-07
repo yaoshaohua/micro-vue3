40c72e35b90a215611f5091d09a9a75951ef3dae
yaoshaohua <eurallyao@163.com>
2022-01-07 18:09:22 +0800

    innit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,36 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
-import router from "./router";
+import routes from "./router";
 
-createApp(App).use(router).mount("#app");
+let router = null;
+let instance:any = null;
+function render(props:any = {}) {
+  const { container } = props;
+  const router = createRouter({
+    history: createWebHistory((window as any).__POWERED_BY_QIANKUN__ ? '/micro-vue3/' : '/'),
+    routes,
+  });
+
+  instance = createApp(App)
+  instance.use(router).mount(container ? container.querySelector('#sub-app-vue3') : '#sub-app-vue3');
+}
+
+// 独立运行时
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log('[vue] vue app bootstraped');
+}
+export async function mount(props:any) {
+  console.log('[vue] props from main framework', props);
+  render(props);
+}
+export async function unmount() {
+  instance.unmount()
+  instance._container.innerHTML = ''
+  instance = null;
+  router = null;
+}
---
+++ src/public-path.js
@@ -0,0 +1,10 @@
+// (function () {
+//   if (window.__POWERED_BY_QIANKUN__) {
+//     __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+//   }
+// })();
+
+if (window.__POWERED_BY_QIANKUN__) {
+  // eslint-disable-next-line no-undef
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
